2c2303c161bb36162d64383b168ab6fe9e5c18e1
TatPhong <tattranphong@gmail.com>
2020-06-10 17:45:20 +0700

    0 bik la cai gi nua

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.textView);
+        btn.OnClickListener(){
+
+        }
+    }
+}
